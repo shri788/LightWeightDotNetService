4d0c6d0c1246c5f73f340cf0a86d1096a54ca867
shri788 <62801295+shri788@users.noreply.github.com>
2022-04-05 10:11:12 +0530

    configuring hangfire scheduler for timly servcies running

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Hangfire;
 using LightWeightDotNetService.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +9,17 @@ builder.Services.AddDbContext<PostDbContext>(c =>
       c.UseNpgsql(builder.Configuration.GetConnectionString("sqlServerConnection")));
 
 //builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDataSyncRepo, DataSyncRepo>();
 
 builder.Services.AddHttpClient("myClient", client =>
 {
     client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
 });
+
+// Add Hangfire services.
+builder.Services.AddHangfire(x => 
+        x.UseSqlServerStorage(builder.Configuration.GetConnectionString("hangFireServerConnection")));
+builder.Services.AddHangfireServer();
 
 // Add services to the container.
 
@@ -25,9 +33,11 @@ if (app.Environment.IsDevelopment())
 }
 
 // Configure the HTTP request pipeline.
-
-app.MapGet("/weatherforecast", async(IHttpClientFactory clientFactory, PostDbContext context) =>
-{
+app.MapGet("/getFromApiEnd", ([FromServices] IDataSyncRepo PostRepo) =>
+{ 
+RecurringJob.AddOrUpdate("FirstJob", () =>
+       PostRepo.DoDbSync(), Cron.Daily);
+    /*);
     foreach (var post in context.Posts)
     {
         context.Remove(post);
@@ -46,8 +56,49 @@ app.MapGet("/weatherforecast", async(IHttpClientFactory clientFactory, PostDbCon
     }
     await context.SaveChangesAsync();
 
-    return result;
+    return result;*/
 });
 
 app.Run();
 
+public class DataSyncRepo: IDataSyncRepo
+{
+    private readonly IHttpClientFactory clientFactory;
+
+    private readonly PostDbContext context;
+
+    public DataSyncRepo(IHttpClientFactory clientFactory, PostDbContext context)
+    {
+        this.clientFactory = clientFactory;
+        this.context = context;
+    }
+
+    public async Task<IEnumerable<Post>> DoDbSync()
+    {
+        foreach (var post in context.Posts)
+        {
+            context.Remove(post);
+        }
+        await context.SaveChangesAsync();
+        var httpClient = clientFactory.CreateClient("myClient");
+        var result = await httpClient.GetFromJsonAsync<List<Post>>("/posts");
+
+        foreach (var post in result)
+        {
+            var postItem = new Post();
+            postItem.UserId = post.UserId;
+            postItem.Title = post.Title;
+            postItem.Body = post.Body;
+            context.Posts.Add(postItem);
+        }
+        await context.SaveChangesAsync();
+
+        return result;
+    }
+
+}
+
+public interface IDataSyncRepo
+{
+    Task<IEnumerable<Post>> DoDbSync();
+}
